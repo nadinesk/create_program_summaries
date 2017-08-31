f1b9dd39c6bbf30b5250c186f3c38af0a1afebd1
nadinesk <nadine.khattak@gmail.com>
2017-08-31 11:23:18 -0400

    replace files

--- cte_r_script.R
+++ cte_r_script.R
@@ -2,20 +2,18 @@
 # http://www.r-bloggers.com/how-to-source-an-r-script-automatically-on-a-mac-using-automator-and-ical/
 # http://www.engadget.com/2013/03/18/triggering-applescripts-from-calendar-alerts-in-mountain-lion/
 
+library(dplyr)
 library(knitr)
 library(markdown)
 library(rmarkdown)
 
-rm(mtcars)
-
-mtcars <- mtcars
 ## knitr loop
 
-names(mtcars)
+
 cte_data <- read.csv("D:/cte_one_pager/demo_s8.csv", stringsAsFactors = FALSE) %>%
                 select(-X)
 
-cte_data1 <- cte_data[1:2,]
+cte_data1 <- cte_data
 
 rownames(cte_data1) <- cte_data1[,1]
 
@@ -23,14 +21,9 @@ cte_data2 <- cte_data1[,-1]
 
 
 for (dbn in unique(rownames(cte_data2))){
-  rmarkdown::render('D:/cte_rmarkdown_script.Rmd', 
+  rmarkdown::render('D:/rpt/cte_markdown_script.Rmd', 
                     output_file =  paste("report_", dbn, '_', Sys.Date(), ".docx", sep=''), 
-                    output_dir = 'D:/rpt')
-  # for pdf reports  
-  #   rmarkdown::render(input = "/Users/majerus/Desktop/R/auto_reporting/test/r_script_pdf.Rmd", 
-  #           output_format = "pdf_document",
-  #           output_file = paste("test_report_", car, Sys.Date(), ".pdf", sep=''),
-  #           output_dir = "/Users/majerus/Desktop/R/auto_reporting/test/reports")
-  
+                    output_dir = 'D:/cte_one_pager/rpts')
+ 
 }
 
